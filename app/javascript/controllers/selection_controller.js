6b51400510d6c88b07383d51b986097de4470fcd
Katherine NewportForbes <katherinenf@gmail.com>
2024-03-07 12:46:59 +0100

    when set changes makes request for words

---
+++ app/javascript/controllers/selection_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="selection"
+export default class extends Controller {
+  static targets = ['dropdown', 'list', 'form']
+  connect() {
+  }
+
+  listWords() {
+    const url = `${this.formTarget.action}/${(this.dropdownTarget.value)}`
+    // const url = word_sets_path(this.dropdownTarget.value)
+    fetch(url, {headers: {"Accept": "text/plain"}})
+      .then(response => response.text())
+      .then((data) => {
+        this.listTarget.outerHTML = data
+      })
+  }
+}
