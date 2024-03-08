a7e12cadd8947495c4e418afa6da3f869f6f00c9
Katherine NewportForbes <katherinenf@gmail.com>
2024-03-08 14:53:44 +0100

    fixed nested word lists

--- app/javascript/controllers/selection_controller.js
+++ app/javascript/controllers/selection_controller.js
@@ -12,7 +12,7 @@ export default class extends Controller {
     fetch(url, {headers: {"Accept": "text/plain"}})
       .then(response => response.text())
       .then((data) => {
-        this.listTarget.outerHTML = data
+        this.listTarget.innerHTML = data
       })
   }
 }
