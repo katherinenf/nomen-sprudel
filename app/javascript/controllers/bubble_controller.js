e3133ecfff259503518be50965ffccd2ae017a90
Katherine NewportForbes <katherinenf@gmail.com>
2024-03-11 11:01:46 +0100

    words in bubbles

--- app/javascript/controllers/bubble_controller.js
+++ app/javascript/controllers/bubble_controller.js
@@ -3,11 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="bubble"
 export default class extends Controller {
   connect() {
-    console.log("Hello");
-    // Add click event listener to the element
-    this.element.addEventListener('click', () => {
-      this.popBubble();
-    });
   }
 
   popBubble() {
