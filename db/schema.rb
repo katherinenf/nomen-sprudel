5c724752ee253c2ea6c3e22cc6f642a80214907c
Katherine NewportForbes <katherinenf@gmail.com>
2024-03-12 09:55:24 +0100

    new seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_11_104335) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_11_171717) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_11_104335) do
     t.bigint "word_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "successes", default: 0
     t.index ["user_id"], name: "index_masteries_on_user_id"
     t.index ["word_id"], name: "index_masteries_on_word_id"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,98 +1,75 @@
 p 'clearing database'
+Mastery.delete_all
 Word.delete_all
 WordSet.delete_all
 
-p 'creating sets'
-animals_word_set = WordSet.create(name: 'animals')
-food_word_set = WordSet.create(name: 'foods')
-household_word_set = WordSet.create(name: 'household')
+# Seed file to populate word sets with sample data
 
-p WordSet.count
+p 'creating words'
+# Create animal words
+animal_words = [
+  { english: "dog", german: "Hund", article: "der" },
+  { english: "cat", german: "Katze", article: "die" },
+  { english: "bird", german: "Vogel", article: "der" },
+  { english: "rabbit", german: "Kaninchen", article: "das" },
+  { english: "horse", german: "Pferd", article: "das" },
+  { english: "fish", german: "Fisch", article: "der" },
+  { english: "turtle", german: "Schildkröte", article: "die" },
+  { english: "lion", german: "Löwe", article: "der" },
+  { english: "elephant", german: "Elefant", article: "der" },
+  { english: "monkey", german: "Affe", article: "der" }
+]
 
-p 'filling sets'
+# Create household object words
+household_object_words = [
+  { english: "chair", german: "Stuhl", article: "der" },
+  { english: "table", german: "Tisch", article: "der" },
+  { english: "bed", german: "Bett", article: "das" },
+  { english: "lamp", german: "Lampe", article: "die" },
+  { english: "bookshelf", german: "Bücherregal", article: "das" },
+  { english: "television", german: "Fernseher", article: "der" },
+  { english: "clock", german: "Uhr", article: "die" },
+  { english: "microwave", german: "Mikrowelle", article: "die" },
+  { english: "refrigerator", german: "Kühlschrank", article: "der" },
+  { english: "vacuum cleaner", german: "Staubsauger", article: "der" }
+]
 
-# create the words for animal set
-Word.create(english: 'dog', german: 'Hund', article: 'der', word_set: animals_word_set)
-Word.create(english: 'cat', german: 'Katze', article: 'die', word_set: animals_word_set)
-Word.create(english: 'elephant', german: 'Elefant', article: 'der', word_set: animals_word_set)
-Word.create(english: 'lion', german: 'Löwe', article: 'der', word_set: animals_word_set)
-Word.create(english: 'tiger', german: 'Tiger', article: 'der', word_set: animals_word_set)
-Word.create(english: 'zebra', german: 'Zebra', article: 'das', word_set: animals_word_set)
-Word.create(english: 'giraffe', german: 'Giraffe', article: 'die', word_set: animals_word_set)
-Word.create(english: 'monkey', german: 'Affe', article: 'der', word_set: animals_word_set)
-Word.create(english: 'hippopotamus', german: 'Flusspferd', article: 'das', word_set: animals_word_set)
-Word.create(english: 'kangaroo', german: 'Känguru', article: 'das', word_set: animals_word_set)
-Word.create(english: 'crocodile', german: 'Krokodil', article: 'das', word_set: animals_word_set)
-Word.create(english: 'penguin', german: 'Pinguin', article: 'der', word_set: animals_word_set)
-Word.create(english: 'polar bear', german: 'Eisbär', article: 'der', word_set: animals_word_set)
-Word.create(english: 'koala', german: 'Koala', article: 'der', word_set: animals_word_set)
-Word.create(english: 'octopus', german: 'Oktopus', article: 'der', word_set: animals_word_set)
-Word.create(english: 'dolphin', german: 'Delfin', article: 'der', word_set: animals_word_set)
-Word.create(english: 'whale', german: 'Wal', article: 'der', word_set: animals_word_set)
-Word.create(english: 'seal', german: 'Robbe', article: 'die', word_set: animals_word_set)
-Word.create(english: 'shark', german: 'Hai', article: 'der', word_set: animals_word_set)
-Word.create(english: 'turtle', german: 'Schildkröte', article: 'die', word_set: animals_word_set)
-Word.create(english: 'snake', german: 'Schlange', article: 'die', word_set: animals_word_set)
-Word.create(english: 'rabbit', german: 'Hase', article: 'der', word_set: animals_word_set)
-Word.create(english: 'fox', german: 'Fuchs', article: 'der', word_set: animals_word_set)
-Word.create(english: 'wolf', german: 'Wolf', article: 'der', word_set: animals_word_set)
-Word.create(english: 'bear', german: 'Bär', article: 'der', word_set: animals_word_set)
+# Create common food words
+food_words = [
+  { english: "apple", german: "Apfel", article: "der" },
+  { english: "banana", german: "Banane", article: "die" },
+  { english: "bread", german: "Brot", article: "das" },
+  { english: "cheese", german: "Käse", article: "der" },
+  { english: "rice", german: "Reis", article: "der" },
+  { english: "chicken", german: "Hähnchen", article: "das" },
+  { english: "potato", german: "Kartoffel", article: "die" },
+  { english: "tomato", german: "Tomate", article: "die" },
+  { english: "milk", german: "Milch", article: "die" },
+  { english: "coffee", german: "Kaffee", article: "der" }
+]
 
-# create the words for foods set
-# Word.create(english: 'apple', german: 'Apfel', article: 'der', word_set: food_word_set)
-Word.create(english: 'banana', german: 'Banane', article: 'die', word_set: food_word_set)
-Word.create(english: 'orange', german: 'Orange', article: 'die', word_set: food_word_set)
-Word.create(english: 'strawberry', german: 'Erdbeere', article: 'die', word_set: food_word_set)
-Word.create(english: 'grape', german: 'Traube', article: 'die', word_set: food_word_set)
-# Word.create(english: 'watermelon', german: 'Wassermelone', article: 'die', word_set: food_word_set)
-# Word.create(english: 'pineapple', german: 'Ananas', article: 'die', word_set: food_word_set)
-# Word.create(english: 'kiwi', german: 'Kiwi', article: 'die', word_set: food_word_set)
-# Word.create(english: 'mango', german: 'Mango', article: 'die', word_set: food_word_set)
-# Word.create(english: 'pear', german: 'Birne', article: 'die', word_set: food_word_set)
-# Word.create(english: 'cherry', german: 'Kirsche', article: 'die', word_set: food_word_set)
-# Word.create(english: 'apricot', german: 'Aprikose', article: 'die', word_set: food_word_set)
-# Word.create(english: 'peach', german: 'Pfirsich', article: 'der', word_set: food_word_set)
-# Word.create(english: 'plum', german: 'Pflaume', article: 'die', word_set: food_word_set)
-# Word.create(english: 'lemon', german: 'Zitrone', article: 'die', word_set: food_word_set)
-# Word.create(english: 'lime', german: 'Limette', article: 'die', word_set: food_word_set)
-# Word.create(english: 'grapefruit', german: 'Grapefruit', article: 'die', word_set: food_word_set)
-# Word.create(english: 'coconut', german: 'Kokosnuss', article: 'die', word_set: food_word_set)
-# Word.create(english: 'melon', german: 'Melone', article: 'die', word_set: food_word_set)
-# Word.create(english: 'raspberry', german: 'Himbeere', article: 'die', word_set: food_word_set)
-# Word.create(english: 'blueberry', german: 'Blaubeere', article: 'die', word_set: food_word_set)
-# Word.create(english: 'blackberry', german: 'Brombeere', article: 'die', word_set: food_word_set)
-# Word.create(english: 'fig', german: 'Feige', article: 'die', word_set: food_word_set)
-# Word.create(english: 'avocado', german: 'Avocado', article: 'die', word_set: food_word_set)
-# Word.create(english: 'tomato', german: 'Tomate', article: 'die', word_set: food_word_set)
-# Word.create(english: 'bread', german: 'Brot', article: 'das', word_set: food_word_set)
-# Word.create(english: 'cheese', german: 'Käse', article: 'der', word_set: food_word_set)
-# Word.create(english: 'chicken', german: 'Hähnchen', article: 'das', word_set: food_word_set)
-
-#create the words for household
-Word.create(english: 'living room', german: 'Wohnzimmer', article: 'das', word_set: household_word_set)
-Word.create(english: 'kitchen', german: 'Küche', article: 'die', word_set: household_word_set)
-Word.create(english: 'bedroom', german: 'Schlafzimmer', article: 'das', word_set: household_word_set)
-Word.create(english: 'bathroom', german: 'Badezimmer', article: 'das', word_set: household_word_set)
-Word.create(english: 'dining room', german: 'Esszimmer', article: 'das', word_set: household_word_set)
-Word.create(english: 'office', german: 'Büro', article: 'das', word_set: household_word_set)
-Word.create(english: 'garage', german: 'Garage', article: 'die', word_set: household_word_set)
-Word.create(english: 'attic', german: 'Dachboden', article: 'der', word_set: household_word_set)
-Word.create(english: 'basement', german: 'Keller', article: 'der', word_set: household_word_set)
-Word.create(english: 'wardrobe', german: 'Kleiderschrank', article: 'der', word_set: household_word_set)
-Word.create(english: 'bed', german: 'Bett', article: 'das', word_set: household_word_set)
-Word.create(english: 'sofa', german: 'Sofa', article: 'das', word_set: household_word_set)
-Word.create(english: 'table', german: 'Tisch', article: 'der', word_set: household_word_set)
-Word.create(english: 'chair', german: 'Stuhl', article: 'der', word_set: household_word_set)
-Word.create(english: 'desk', german: 'Schreibtisch', article: 'der', word_set: household_word_set)
-Word.create(english: 'bookshelf', german: 'Bücherregal', article: 'das', word_set: household_word_set)
-Word.create(english: 'cabinet', german: 'Schrank', article: 'der', word_set: household_word_set)
-Word.create(english: 'lamp', german: 'Lampe', article: 'die', word_set: household_word_set)
-Word.create(english: 'mirror', german: 'Spiegel', article: 'der', word_set: household_word_set)
-Word.create(english: 'rug', german: 'Teppich', article: 'der', word_set: household_word_set)
-Word.create(english: 'curtain', german: 'Vorhang', article: 'der', word_set: household_word_set)
-Word.create(english: 'shelf', german: 'Regal', article: 'das', word_set: household_word_set)
-Word.create(english: 'stove', german: 'Herd', article: 'der', word_set: household_word_set)
-Word.create(english: 'refrigerator', german: 'Kühlschrank', article: 'der', word_set: household_word_set)
-Word.create(english: 'sink', german: 'Spüle', article: 'die', word_set: household_word_set)
+# Create word sets
+animal_set = WordSet.create(name: "Animals")
+household_object_set = WordSet.create(name: "Household Objects")
+food_set = WordSet.create(name: "Food")
 
 p Word.count
+p WordSet.count
+
+p 'adding words to sets'
+# Add words to word sets
+animal_words.each do |word|
+  animal_set.words.create(word)
+end
+
+household_object_words.each do |word|
+  household_object_set.words.create(word)
+end
+
+food_words.each do |word|
+  food_set.words.create(word)
+end
+
+p animal_set.words.count
+p household_object_set.words.count
+p food_set.words.count
